eb56b2013516fa88d10f727afa41049a803b9c74
Rinat <nutsgamedev@gmail.com>
2014-09-14 14:40:34 +0600

    add Insertion Sort

---
+++ test/sortSpec.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Rinat on 14.09.14.
+ */
+var expect = require("chai").expect;
+
+var algorithms = require('../index');
+var sort = algorithms.sort;
+
+describe('sort', function () {
+    // run before every test
+    beforeEach(function () {
+
+    });
+
+    it('insertionSort', function () {
+        var unSortedArray = [5, 2, 4, 6, 1, 3];
+        var sortedArray = [1, 2, 3, 4, 5, 6];
+
+        var test = sort.insertionSort(unSortedArray);
+        expect(test).to.deep.equal(sortedArray);
+    });
+});
