8591056561618f544a09614fd67d5a814283b1f9
RinatMullayanov <nutsgamedev@gmail.com>
2014-09-15 15:07:21 +0600

    add MergeSort

--- test/sortSpec.js
+++ test/sortSpec.js
@@ -19,4 +19,13 @@ describe('sort', function () {
         var test = sort.insertionSort(unSortedArray);
         expect(test).to.deep.equal(sortedArray);
     });
+
+    it('mergeSort', function () {
+        var unSortedArray = [5, 2, 4, 6, 1, 3];
+        var sortedArray = [1, 2, 3, 4, 5, 6];
+
+        var test = sort.mergeSort(unSortedArray);
+        expect(test).to.deep.equal(sortedArray);
+    });
+
 });
